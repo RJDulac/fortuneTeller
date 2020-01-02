7c4bde729c4cef364bee3a66323bb9f7f6c04266
machine <analyticrye@gmail.com>
2020-01-02 05:21:53 -0500

    need to add more fortunes

---
+++ fortune.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+enum fortune
+{
+    money,
+    love,
+    job
+
+};
+void getFortune()
+{
+    srand(time(NULL));
+    int random = rand() % 3;
+    switch (fortune(random))
+    {
+    case money:
+        cout << "You will recieve large sums of money." << endl;
+        break;
+    case love:
+        cout << "You will find true love." << endl;
+        break;
+    case job:
+        cout << "You will get a promotion at work!" << endl;
+        break;
+    default:
+        cout << "something went wrong";
+    }
+}
+int main()
+{
+
+    bool isTelling = true;
+    char input = ' ';
+    while (isTelling)
+    {
+        if (input != 'y')
+        {
+            cout << "Press enter to get your fortune!" << endl;
+            cin.get(input);
+            getFortune();
+        }
+        cout << "Enter y if you want another fortune or n if you don't want another fortune." << endl;
+        cin >> input;
+        if (input == 'n')
+        {
+            isTelling = false;
+        }
+        else
+        {
+            getFortune();
+        }
+    }
+
+    return 0;
+}
